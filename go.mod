98fe8e0f0d587d61cd180afd795c72ba9d0309bb
santoshkal <ksantosh@intelops.dev>
2023-11-27 16:42:26 +0530

    Add tf validation with Rego
    Signed-off-by: santoshkal <ksantosh@intelops.dev>

--- go.mod
+++ go.mod
@@ -12,7 +12,10 @@ require (
 
 require (
 	github.com/OneOfOne/xxhash v1.2.8 // indirect
+	github.com/agext/levenshtein v1.2.1 // indirect
 	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/cockroachdb/apd/v3 v3.2.0 // indirect
@@ -29,6 +32,7 @@ require (
 	github.com/google/uuid v1.3.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.19.1 // indirect
 	github.com/jedib0t/go-pretty/v6 v6.4.9 // indirect
 	github.com/klauspost/compress v1.17.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -49,6 +53,7 @@ require (
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/yashtewari/glob-intersection v0.2.0 // indirect
+	github.com/zclconf/go-cty v1.13.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.45.0 // indirect
 	go.opentelemetry.io/otel v1.19.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.19.0 // indirect
---
+++ pkg/parser/tfparser.go
@@ -0,0 +1,319 @@
+package parser
+
+/*
+	Following code has been borrowed and modified from:
+	https://github.com/tmccombs/hcl2json/blob/5c1402dc2b410e362afee45a3cf15dcb08bc1f2c/convert.go
+*/
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	hcl "github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/zclconf/go-cty/cty"
+	ctyconvert "github.com/zclconf/go-cty/cty/convert"
+	ctyjson "github.com/zclconf/go-cty/cty/json"
+)
+
+type jsonObj map[string]interface{}
+type lineObj map[string]interface{}
+
+type converter struct {
+	bytes []byte
+}
+
+func (c *converter) rangeSource(r hcl.Range) string {
+	return string(c.bytes[r.Start.Byte:r.End.Byte])
+}
+
+func (c *converter) convertBody(body *hclsyntax.Body) (jsonObj, lineObj, error) {
+
+	var (
+		err  error
+		cfg  = make(jsonObj) // resource config
+		lcfg = make(lineObj) // resource line config
+	)
+
+	// convert attributes
+	for key, value := range body.Attributes {
+		cfg[key], lcfg[key], err = c.convertExpression(value.Expr)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	// convert blocks (nested objects, lists)
+	for _, block := range body.Blocks {
+
+		var (
+			bcfg  = make(jsonObj) // block resource config
+			blcfg = make(lineObj) // block resource line config
+		)
+
+		err = c.convertBlock(block, bcfg, blcfg)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		blockConfig := bcfg[block.Type].(jsonObj)
+		lineCfg := blcfg[block.Type].(lineObj)
+		if _, present := cfg[block.Type]; !present {
+			cfg[block.Type] = []jsonObj{blockConfig}
+			lcfg[block.Type] = []lineObj{lineCfg}
+		} else {
+			list := cfg[block.Type].([]jsonObj)
+			list = append(list, blockConfig)
+			cfg[block.Type] = list
+
+			lineList := lcfg[block.Type].([]lineObj)
+			lineList = append(lineList, lineCfg)
+			lcfg[block.Type] = lineList
+		}
+	}
+
+	return cfg, lcfg, nil
+}
+
+func (c *converter) convertBlock(block *hclsyntax.Block, cfg jsonObj, lcfg lineObj) error {
+	var key string = block.Type
+
+	value, blcfg, err := c.convertBody(block.Body)
+	if err != nil {
+		return err
+	}
+
+	for _, label := range block.Labels {
+		if inner, exists := cfg[key]; exists {
+			var ok bool
+			cfg, ok = inner.(jsonObj)
+			if !ok {
+				// TODO: better diagnostics
+				return fmt.Errorf("unable to convert Block to JSON: %v.%v", block.Type, strings.Join(block.Labels, "."))
+			}
+
+			if innerLineObj := lcfg[key]; exists {
+				lcfg, ok = innerLineObj.(lineObj)
+				if !ok {
+					return fmt.Errorf("unable to convert Block to JSON: %v.%v", block.Type, strings.Join(block.Labels, "."))
+				}
+			}
+
+		} else {
+			var (
+				obj  = make(jsonObj)
+				lobj = make(lineObj)
+			)
+
+			cfg[key] = obj
+			cfg = obj
+
+			lcfg[key] = lobj
+			lcfg = lobj
+
+		}
+		key = label
+	}
+
+	// resource config for blocks
+	if current, exists := cfg[key]; exists {
+		if list, ok := current.([]interface{}); ok {
+			cfg[key] = append(list, value)
+		} else {
+			cfg[key] = []interface{}{current, value}
+		}
+	} else {
+		cfg[key] = value
+	}
+
+	// resource line config for blocks
+	if current, exists := lcfg[key]; exists {
+		if list, ok := current.([]interface{}); ok {
+			lcfg[key] = append(list, blcfg)
+		} else {
+			lcfg[key] = []interface{}{current, blcfg}
+		}
+	} else {
+		lcfg[key] = blcfg
+	}
+
+	return nil
+}
+
+func (c *converter) convertExpression(expr hclsyntax.Expression) (ret interface{}, line interface{}, err error) {
+	// assume it is hcl syntax (because, um, it is)
+	line = expr.StartRange().Start.Line
+	switch value := expr.(type) {
+	case *hclsyntax.LiteralValueExpr:
+		return ctyjson.SimpleJSONValue{Value: value.Val}, line, nil
+	case *hclsyntax.TemplateExpr:
+		ret, err = c.convertTemplate(value)
+		return
+	case *hclsyntax.TemplateWrapExpr:
+		return c.convertExpression(value.Wrapped)
+	case *hclsyntax.TupleConsExpr:
+		var list []interface{}
+		for _, ex := range value.Exprs {
+			elem, line, err := c.convertExpression(ex)
+			if err != nil {
+				return nil, line, err
+			}
+			list = append(list, elem)
+		}
+		return list, line, nil
+	case *hclsyntax.ObjectConsExpr:
+		m := make(jsonObj)
+		l := make(lineObj)
+		for _, item := range value.Items {
+			key, err := c.convertKey(item.KeyExpr)
+			if err != nil {
+				return nil, line, err
+			}
+			m[key], l[key], err = c.convertExpression(item.ValueExpr)
+			if err != nil {
+				return nil, line, err
+			}
+		}
+		return m, l, nil
+	default:
+		return c.wrapExpr(expr), line, nil
+	}
+}
+
+func (c *converter) convertTemplate(t *hclsyntax.TemplateExpr) (string, error) {
+	if t.IsStringLiteral() {
+		// safe because the value is just the string
+		v, err := t.Value(nil)
+		if err != nil {
+			return "", err
+		}
+		return v.AsString(), nil
+	}
+	var builder strings.Builder
+	for _, part := range t.Parts {
+		s, err := c.convertStringPart(part)
+		if err != nil {
+			return "", err
+		}
+		builder.WriteString(s)
+	}
+	return builder.String(), nil
+}
+
+func (c *converter) convertStringPart(expr hclsyntax.Expression) (string, error) {
+	switch v := expr.(type) {
+	case *hclsyntax.LiteralValueExpr:
+		s, err := ctyconvert.Convert(v.Val, cty.String)
+		if err != nil {
+			return "", err
+		}
+		return s.AsString(), nil
+	case *hclsyntax.TemplateExpr:
+		return c.convertTemplate(v)
+	case *hclsyntax.TemplateWrapExpr:
+		return c.convertStringPart(v.Wrapped)
+	case *hclsyntax.ConditionalExpr:
+		return c.convertTemplateConditional(v)
+	case *hclsyntax.TemplateJoinExpr:
+		return c.convertTemplateFor(v.Tuple.(*hclsyntax.ForExpr))
+	default:
+		// treating as an embedded expression
+		return c.wrapExpr(expr), nil
+	}
+}
+
+func (c *converter) convertKey(keyExpr hclsyntax.Expression) (string, error) {
+	// a key should never have dynamic input
+	if k, isKeyExpr := keyExpr.(*hclsyntax.ObjectConsKeyExpr); isKeyExpr {
+		keyExpr = k.Wrapped
+		if _, isTraversal := keyExpr.(*hclsyntax.ScopeTraversalExpr); isTraversal {
+			return c.rangeSource(keyExpr.Range()), nil
+		}
+	}
+	return c.convertStringPart(keyExpr)
+}
+
+func (c *converter) convertTemplateConditional(expr *hclsyntax.ConditionalExpr) (string, error) {
+	var builder strings.Builder
+	builder.WriteString("%{if ")
+	builder.WriteString(c.rangeSource(expr.Condition.Range()))
+	builder.WriteString("}")
+	trueResult, err := c.convertStringPart(expr.TrueResult)
+	if err != nil {
+		return "", nil
+	}
+	builder.WriteString(trueResult)
+	falseResult, _ := c.convertStringPart(expr.FalseResult)
+	if len(falseResult) > 0 {
+		builder.WriteString("%{else}")
+		builder.WriteString(falseResult)
+	}
+	builder.WriteString("%{endif}")
+
+	return builder.String(), nil
+}
+
+func (c *converter) convertTemplateFor(expr *hclsyntax.ForExpr) (string, error) {
+	var builder strings.Builder
+	builder.WriteString("%{for ")
+	if len(expr.KeyVar) > 0 {
+		builder.WriteString(expr.KeyVar)
+		builder.WriteString(", ")
+	}
+	builder.WriteString(expr.ValVar)
+	builder.WriteString(" in ")
+	builder.WriteString(c.rangeSource(expr.CollExpr.Range()))
+	builder.WriteString("}")
+	templ, err := c.convertStringPart(expr.ValExpr)
+	if err != nil {
+		return "", err
+	}
+	builder.WriteString(templ)
+	builder.WriteString("%{endfor}")
+
+	return builder.String(), nil
+}
+
+func (c *converter) wrapExpr(expr hclsyntax.Expression) string {
+	return "${" + c.rangeSource(expr.Range()) + "}"
+}
+
+// ConvertTFtoJSON reads a Terraform file and converts it to JSON format.
+func ConvertTFtoJSON(tfFilePath string) (string, error) {
+	// Read the Terraform file
+	tfData, err := os.ReadFile(tfFilePath)
+	if err != nil {
+		return "", err
+	}
+
+	// Parse the Terraform file
+	file, diags := hclsyntax.ParseConfig(tfData, tfFilePath, hcl.Pos{Byte: 0, Line: 1, Column: 1})
+	if diags.HasErrors() {
+		return "", diags
+	}
+
+	// Create a converter instance
+	c := converter{bytes: tfData}
+
+	// Ensure that file.Body is of the correct type
+	hclBody, ok := file.Body.(*hclsyntax.Body)
+	if !ok {
+		return "", fmt.Errorf("file body is not of type *hclsyntax.Body")
+	}
+
+	// Convert the body of the file
+	cfg, _, err := c.convertBody(hclBody)
+	if err != nil {
+		return "", err
+	}
+
+	// Convert the config to JSON
+	jsonBytes, err := json.Marshal(cfg)
+	if err != nil {
+		return "", err
+	}
+
+	return string(jsonBytes), nil
+}
