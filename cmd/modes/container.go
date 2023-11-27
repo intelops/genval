b433830cf5abe522ecf6f0abbe74ba1626548fa4
santoshkal <ksantosh@intelops.dev>
2023-11-27 16:35:09 +0530

    Refactor cmd directory and add k8s rego validation
    Signed-off-by: santoshkal <ksantosh@intelops.dev>

---
+++ cmd/modes/container.go
@@ -0,0 +1,72 @@
+package modes
+
+import (
+	"fmt"
+	"os"
+
+	generate "github.com/intelops/genval/pkg/generate/dockerfile_gen"
+	"github.com/intelops/genval/pkg/parser"
+	"github.com/intelops/genval/pkg/utils"
+	"github.com/intelops/genval/pkg/validate"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	// Set up logrus formatting here if needed
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: false,
+	})
+}
+
+func ExecuteContainer(value, output, inputpolicy, outputpolicy string) {
+	if value == "" || output == "" || inputpolicy == "" || outputpolicy == "" {
+		fmt.Println("[USAGE]: ./genval --mode=container --reqinput=input.json --output=output.Dockerfile --inputpolicy=<path/to/input.rego policy> --outputpolicy <path/tp/output.rego file>")
+		return
+	}
+
+	// inputPath := args[0]
+	// outputPath := args[1]
+	inputPath := value
+	outputPath := output
+	inputPolicyFile := inputpolicy
+	outputPolicyFile := outputpolicy
+
+	// Use ParseInputFile to read and unmarshal the input file
+	var data generate.DockerfileContent
+
+	err := parser.ParseDockerfileInput(inputPath, &data)
+	if err != nil {
+		log.Error("Error parsing Dockerfile input:", err)
+		return
+	}
+
+	inputContent, err := utils.ReadPolicyFile(inputPath)
+	if err != nil {
+		log.Fatalf("Unable to read input: %v", err)
+	}
+
+	// Validate the YAML using OPA
+	err = validate.ValidateInput(string(inputContent), inputPolicyFile)
+	if err != nil {
+		log.Fatalf("Validation error: %v", err)
+		return
+	}
+
+	dockerfileContent := generate.GenerateDockerfileContent(&data)
+
+	outputData := []byte(dockerfileContent)
+	err = os.WriteFile(outputPath, outputData, 0644)
+	if err != nil {
+		log.Error("Error writing Dockerfile:", err)
+		return
+	}
+	fmt.Printf("Generated Dockerfile saved to: %s\n", outputPath)
+
+	err = validate.ValidateDockerfile(string(outputData), outputPolicyFile)
+	if err != nil {
+		log.Error("Dockerfile validation failed:", err)
+		return
+	} else {
+		fmt.Printf("Dockerfile validation succeeded!\n")
+	}
+}
---
+++ pkg/validate/regoval_test.go
@@ -0,0 +1,55 @@
+package validate
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type validateWithRegoTestCase struct {
+	name          string
+	inputContent  string
+	regoPolicy    string
+	expectedError bool
+}
+
+func TestValidateWithRego(t *testing.T) {
+	testdataDir := "./testdata"
+	testCases := []validateWithRegoTestCase{
+		{
+			name:          "valid input and policy",
+			inputContent:  "deployment.json",
+			regoPolicy:    "/rego/k8s.rego",
+			expectedError: false,
+		},
+		{
+			name:          "valid input and invalid policy",
+			inputContent:  "deployment.json",
+			regoPolicy:    "/rego/k8s-invalid.rego",
+			expectedError: true,
+		},
+		{
+			name:          "Invalid input and valid policy",
+			inputContent:  "deployment-invalid.json",
+			regoPolicy:    "/rego/k8s.rego",
+			expectedError: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			inputFilePath := filepath.Join(testdataDir, tc.inputContent)
+			regoPolicyPath := filepath.Join(testdataDir, tc.regoPolicy)
+
+			// Call the function under test
+			err := ValidateWithRego(inputFilePath, regoPolicyPath)
+
+			if tc.expectedError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
