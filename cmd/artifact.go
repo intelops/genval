6e71976d93b41634119200b95dcece23cb374177
Santosh Kaluskar <ksantosh@intelops.dev>
2024-03-06 20:27:06 +0530

    Add artifact subcommand, for OCI operations
    Signed-off-by: Santosh Kaluskar <ksantosh@intelops.dev>

---
+++ cmd/artifact.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var artifactCmd = &cobra.Command{
+	Use:   "artifact",
+	Short: "Command that manages building and pushing OCI artifacts",
+	Long: `
+artifact command provides capabilities for building and pushing of the OCI complient artifacts built form the
+generated/validated config files using Genval.
+
+Currently, Genval provides artifact build, push, and pull commands. Pull and Push commands provides functionality
+to sign and verify the artifacts using the Sigstore cosign tool.
+`,
+}
+
+func init() {
+	rootCmd.AddCommand(artifactCmd)
+}
