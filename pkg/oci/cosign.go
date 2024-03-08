2a2e1f8921bba656eae098f8f90dd1781e4f2a08
Santosh Kaluskar <ksantosh@intelops.dev>
2024-03-08 20:53:34 +0530

    Test Artifact verification - Failing verification
    Signed-off-by: Santosh Kaluskar <ksantosh@intelops.dev>

--- pkg/oci/cosign.go
+++ pkg/oci/cosign.go
@@ -2,11 +2,19 @@ package oci
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"io"
 	"os"
 	"os/exec"
 
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/sigstore/cosign/v2/cmd/cosign/cli/fulcio"
+	"github.com/sigstore/cosign/v2/pkg/cosign"
+	ociremote "github.com/sigstore/cosign/v2/pkg/oci/remote"
+	sig "github.com/sigstore/cosign/v2/pkg/signature"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -58,4 +66,72 @@ func processCosignIO(cosignCmd *exec.Cmd) error {
 	}
 
 	return nil
+}
+
+// var rekorURL = flag.String("rekor-url", defaultRekorURL, "specify Rekor URL")
+
+// var defaultRekorURL = "https://rekor.sigstore.dev"
+
+func VerifyArifact(ctx context.Context, key string, url string, co *cosign.CheckOpts) (verified bool, err error) {
+	// var fulcioIntermediates *x509.CertPool
+	ref, err := name.ParseReference(url)
+	if err != nil {
+		return false, err
+	}
+
+	_, bundleVerified, err := cosign.VerifyImageSignatures(ctx, ref, co)
+	if err != nil {
+		return false, err
+	}
+	if bundleVerified {
+		log.Printf("Artifact %s verified succussfully", url)
+	}
+	return true, nil
+}
+
+// var rekorURL = flag.String("rekor-url", defaultRekorURL, "specify Rekor URL")
+// var fulcioIntermediates *x509.CertPool
+// var defaultRekorURL = "https://rekor.sigstore.dev"
+
+func BuildCosignCheckOpts(ctx context.Context, key string) (*cosign.CheckOpts, error) {
+	opts := []remote.Option{
+		remote.WithAuthFromKeychain(authn.DefaultKeychain),
+		remote.WithContext(ctx),
+	}
+
+	rootCerts, err := fulcio.GetRoots()
+	if err != nil {
+		return nil, fmt.Errorf("getting Fulcio roots: %w", err)
+	}
+
+	intermediateCerts, err := fulcio.GetIntermediates()
+	if err != nil {
+		return nil, fmt.Errorf("getting Fulcio intermediates: %w", err)
+	}
+
+	co := &cosign.CheckOpts{
+		ClaimVerifier:      cosign.SimpleClaimVerifier,
+		IntermediateCerts:  intermediateCerts,
+		RootCerts:          rootCerts,
+		RegistryClientOpts: []ociremote.Option{ociremote.WithRemoteOptions(opts...)},
+	}
+
+	fulcioVerified := co.SigVerifier == nil
+	log.Printf("FulCio Verified: %v", fulcioVerified)
+
+	// Check if PubKey is supplied
+	if key != "" {
+		pub, err := sig.LoadPublicKey(ctx, key)
+		if err != nil {
+			log.Errorf("Error loading Pub Key: %v", err)
+			return nil, err
+		}
+		co.SigVerifier = pub
+	}
+
+	co.IgnoreTlog = true
+	co.IgnoreSCT = true
+	co.Offline = true
+
+	return co, nil
 }
