04601bd92b80709dbdabc3b4e569886001513fad
Santosh Kaluskar <ksantosh@intelops.dev>
2024-03-28 16:59:03 +0530

    WIP: update auth workflow to authenticate with registries
    Now, Pull command looks for and uses DefaultKeychain in '~/.docker/config.json.
    if not found, fallbacks to username:password format geting the data from env variables
    
    Signed-off-by: Santosh Kaluskar <ksantosh@intelops.dev>

--- cmd/artifact.go
+++ cmd/artifact.go
@@ -13,6 +13,14 @@ generated/validated config files using Genval.
 
 Currently, Genval provides artifact build, push, and pull commands. Pull and Push commands provides functionality
 to sign and verify the artifacts using the Sigstore cosign tool.
+
+Authentication is necessary for all commands related to managing artifacts within registries. By default,
+the system retrieves credentials from the default keychain located at "~/.docker/config.json".
+However, if such a keychain is not available, users can alternatively provide their own credentials using environment variables.
+To do so, set the required credentials as environment variables in the following format:
+
+export ARTIFACT_REGISTRY_USERNAME=<your username>
+export ARTIFACT_REGISTRY_PASSWORD=<your password>
 `,
 }
 
