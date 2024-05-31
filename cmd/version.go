773d24876800694fc4e9e7c256f91d115bef8962
Santosh Kaluskar <141515226+santoshkal@users.noreply.github.com>
2024-05-31 19:13:42 +0530

    Fix version command (#81)
    * Fix: fix version command using k8s release-utils
    
    Signed-off-by: Santosh <ksantosh@intelops.dev>
    
    * Update goreleaser.yaml
    
    Signed-off-by: Santosh <ksantosh@intelops.dev>
    
    ---------
    
    Signed-off-by: Santosh <ksantosh@intelops.dev>

--- cmd/version.go
+++ cmd/version.go
@@ -2,12 +2,9 @@ package cmd
 
 import (
 	"fmt"
-	"runtime"
 
-	"github.com/containerd/containerd/platforms"
-	"github.com/fatih/color"
-	"github.com/intelops/genval/pkg/utils"
 	"github.com/spf13/cobra"
+	"sigs.k8s.io/release-utils/version"
 )
 
 var versionCmd = &cobra.Command{
@@ -15,29 +12,11 @@ var versionCmd = &cobra.Command{
 	Short: "Prints Genval version information",
 	Long:  `All software has versions. This is Genval's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		version, commitHash, buildTime, err := utils.FetchVersionInfo()
+		vCmd := version.Version()
+		err := vCmd.RunE(cmd, args)
 		if err != nil {
-			return
+			fmt.Println("Error:", err)
 		}
-		color.New(color.FgHiCyan, color.Bold).Println(`
-		:'######:::'########:'##::: ##:'##::::'##::::'###::::'##:::::::
-		:##... ##:: ##.....:: ###:: ##: ##:::: ##:::'## ##::: ##:::::::
-		:##:::..::: ##::::::: ####: ##: ##:::: ##::'##:. ##:: ##:::::::
-		:##::'####: ######::: ## ## ##: ##:::: ##:'##:::. ##: ##:::::::
-		:##::: ##:: ##...:::: ##. ####:. ##:: ##:: #########: ##:::::::
-		:##::: ##:: ##::::::: ##:. ###::. ## ##::: ##.... ##: ##:::::::
-		: ######::: ########: ##::. ##:::. ###:::: ##:::: ##: ########:
-		:......::::........::..::::..:::::...:::::..:::::..::........::
-
-		genval is a CLI tool to generate and validate configuration files
-
-		`)
-		fmt.Printf("Version:\t%s\n", version)
-		fmt.Printf("Git Commit:\t%s\n", commitHash)
-		fmt.Printf("OS/Arch:\t%s/%s\n", runtime.GOOS, runtime.GOARCH)
-		fmt.Printf("Go version:\t%s\n", runtime.Version())
-		fmt.Printf("Build date:\t%s\n", buildTime)
-		fmt.Printf("Build platform:\t%s\n", platforms.DefaultString())
 	},
 }
 
