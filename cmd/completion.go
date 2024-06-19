9e63a0d05f8b549e6d8e882630845967533cadf6
santoshkal <ksantosh@intelops.dev>
2024-06-19 14:38:11 +0530

    Fix: lint errors
    Signed-off-by: santoshkal <ksantosh@intelops.dev>

--- cmd/completion.go
+++ cmd/completion.go
@@ -58,13 +58,13 @@ PowerShell:
 	Run: func(cmd *cobra.Command, args []string) {
 		switch args[0] {
 		case "bash":
-			cmd.Root().GenBashCompletion(os.Stdout)
+			rootCmd.GenBashCompletion(os.Stdout)
 		case "zsh":
-			cmd.Root().GenZshCompletion(os.Stdout)
+			rootCmd.GenZshCompletion(os.Stdout)
 		case "fish":
-			cmd.Root().GenFishCompletion(os.Stdout, true)
+			rootCmd.GenFishCompletion(os.Stdout, true)
 		case "powershell":
-			cmd.Root().GenPowerShellCompletionWithDesc(os.Stdout)
+			rootCmd.GenPowerShellCompletionWithDesc(os.Stdout)
 		}
 	},
 }
