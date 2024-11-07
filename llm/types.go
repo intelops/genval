82a90fc7f579d1c22b71e92100c44fe098423ef8
Santosh <ksantosh@intelops.dev>
2024-11-07 18:08:53 +0530

    Updated structs aligning YAML structure

--- llm/types.go
+++ llm/types.go
@@ -8,56 +8,47 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-// Config represents the top-level configuration structure
 type Config struct {
 	APIVersion      string          `yaml:"apiVersion"`
 	Metadata        Metadata        `yaml:"metadata"`
 	RequirementSpec RequirementSpec `yaml:"requirementSpec"`
 }
 
-// Metadata holds metadata information
 type Metadata struct {
 	Name string `yaml:"name"`
 }
 
-// LLMSpec contains the specifications for LLM
 type RequirementSpec struct {
-	Common    CommonSpec `yaml:"common"`
-	LLMParams LLMSpec    `yaml:"llmSpec"`
+	Common  CommonSpec `yaml:"common"`
+	LLMSpec LLMSpec    `yaml:"llmSpec"`
 }
 
-// CommonSpec holds common specifications
 type CommonSpec struct {
 	UserPrompt       string `yaml:"userPrompt"`
-	UserSystemPrompt string `yaml:"userSystemPrompt,omitempty"` // Optional field
-	Assistant        string `yaml:"assistant,omitempty"`
-	Output           string `yaml:"output,omitempty"`
-	Model            string `yaml:"model"`
+	Assistant        string `yaml:"assistant"`
+	UserSystemPrompt string `yaml:"userSystemPrompt,omitempty"`
 }
 
-// SpecificLLMSpec holds specific configurations for different LLMs
 type LLMSpec struct {
-	OpenAIConfig *OpenAIConfig `yaml:"openAIConfig,omitempty"` // Optional field
-	OllamaConfig *OllamaConfig `yaml:"ollamaConfig,omitempty"` // Optional field
+	OpenAIConfig []OpenAIModel `yaml:"openAIConfig"`
+	OllamaSpec   []OllamaModel `yaml:"ollamaSpec"`
 }
 
-// OpenAIConfig holds configuration for OpenAI
-type OpenAIConfig struct {
-	Assistant        string  `yaml:"assistant,omitempty"`   // Optional field
-	APIKey           string  `yaml:"apiKey"`                // Required field
-	Temperature      float32 `yaml:"temperature,omitempty"` // Optional with default
-	TopP             float32 `yaml:"topP,omitempty"`        // Optional with default
-	Streaming        bool    `yaml:"streaming,omitempty"`   // Optional with default
-	MaxTokens        int     `yaml:"maxTokens,omitempty"`   // Optional with default
-	PresencePenalty  float32 `yaml:"presencePenalty,omitempty"`
-	FrequencyPenalty float32 `yaml:"frequencyPenalty,omitempty"`
+type OpenAIModel struct {
+	Model       string  `yaml:"model"`
+	UseTheModel bool    `yaml:"useTheModel"`
+	APIKey      string  `yaml:"apiKey"`
+	Temperature float32 `yaml:"temperature"`
+	TopP        float32 `yaml:"topP"`
+	Streaming   bool    `yaml:"streaming"`
+	MaxTokens   int     `yaml:"maxTokens"`
 }
 
-// OllamaConfig holds configuration for Ollama
-type OllamaConfig struct {
-	Assistant         string        `yaml:"assistant,omitempty"` // Optional field
-	Endpoint          string        `yaml:"endpoint"`
-	KeepAliveDuration time.Duration `yaml:"keepAliveDuration,omitempty"` // Optional field
+type OllamaModel struct {
+	Model       string        `yaml:"model"`
+	UseTheModel bool          `yaml:"useTheModel"`
+	Endpoint    string        `yaml:"endpoint"`
+	KeepAlive   time.Duration `yaml:"keepAlive,omitempty"`
 }
 
 type OllamaEndpoint struct {
